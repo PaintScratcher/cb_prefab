aff9fff04ca2bdf90ef9f7f80f55b2f6bc780abc
Adam Taylor <adamt@couchbase.com>
2014-07-14 10:14:04 +0100

    Started generating vagrant file and added some more input parsing

--- prefab.py
+++ prefab.py
@@ -3,8 +3,12 @@
 
 # READMEEE
 
+# IMPORTS
 import argparse
+import os
+import sys
 
+# FLAGS
 parser = argparse.ArgumentParser(description='Automatically generate vagrant configurations')
 parser.add_argument("-os", '--os', help='desired operating system', metavar='')
 parser.add_argument("-n", "--nodes", help='number of nodes to provision', metavar='')
@@ -13,7 +17,7 @@ parser.add_argument("-a", "-ask", help='provide cli input for all options', meta
 
 args = parser.parse_args()
 
-#Define Variables and take user input if no flags set
+# Define Variables and take user input if no flags set
 
 if args.v:
 	version = args.v
@@ -36,14 +40,33 @@ elif args.a:
 else:
 	OSname = 'Ubuntu14'
 
+if 'ubuntu' in OSname.lower():
+	if '10' in OSname:
+		OSname = 'Ubuntu10'
+	elif '12' in OSname:
+		OSname = 'Ubuntu12'
+	elif '14' in OSname:
+		OSname = 'Ubuntu14'
+	else:
+		print 'Unrecognised OS'
+		sys.exit(0)
+elif 'rhel' in OSname.lower() or 'centos' in OSname.lower():
+	if '5' in OSname:
+		OSname = 'CentOS5'
+	elif '6' in OSname:
+		OSname = 'CentOS6'
+	else:
+		print 'Unrecognised OS'
+		sys.exit(0)
+
 ip_address_base = "192.168.71.10%d"
 box = "puppetlabs/centos-6.5-64-puppet"
 
 print "Created a " + no_of_nodes + " node cluster running cb version " + version + " on " + OSname
-#Export files
+
 
 #Vagrant File
-lines = ['# Couchbase Server Clustering vagrant file.',
+lines_vagr = ['# Couchbase Server Clustering vagrant file.',
 				'# Generated automatically by prefab!\n',
 				'Vagrant.configure("2") do |config|\n',
 				'	# Server version',
@@ -72,5 +95,30 @@ lines = ['# Couchbase Server Clustering vagrant file.',
 
 				]
 
+# Manifest File
+lines_mani = ['# ===',
+							'# Install and Run Couchbase Server',
+							'# ===\n',
+							'$version = "' + version + '"',
+							#'$stem = "' + stem + '"',
+							'}'
+
+
+				]
+
+
+#Export files
 with open('Vagrantfile', 'w') as f:
-	f.write('\n'.join(lines))
+	f.write('\n'.join(lines_vagr))
+
+filename = "./manifests/default.pp"
+dir = os.path.dirname(filename)
+
+try:
+	os.stat(dir)
+except:
+	print "No directory " + dir
+	os.mkdir(dir)
+
+with open (filename, 'w') as f:
+	f.write('\n'.join(lines_mani))
